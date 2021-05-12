58c33b3e9200e265d66753ac3e7b944c89a75d3d
Travis CI User <travis@example.org>
2021-05-12 10:33:50 +0000

    Bump postcss from 7.0.31 to 8.2.10 (#38)

---
+++ js/446.e7c794a43e5109acde13.js
@@ -0,0 +1,2 @@
+(self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
+//# sourceMappingURL=../sourcemaps/446.1ff638a87560ba98ce06.js.map
