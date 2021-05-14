fdf4af60a6b3c7561fe72d7b6a5160f14c1a5bf1
Travis CI User <travis@example.org>
2021-05-14 08:17:46 +0000

    Add filter for edge devices only (#40)

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.2fb71a519a185a3117bf.js.map
+//# sourceMappingURL=../sourcemaps/446.b68c38e01cc6614905ec.js.map
