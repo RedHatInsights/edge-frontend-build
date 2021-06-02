5708b95f4cd0a5413ac930951b77db03df18c00a
Travis CI User <travis@example.org>
2021-06-02 13:37:19 +0000

    Merge pull request #49 from boaz0/packages_group_label
    Added package labels in the expanded section

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.e739279a33954ded77fa.js.map
+//# sourceMappingURL=../sourcemaps/446.4177646b8e2463b0fe69.js.map
