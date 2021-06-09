9210ca4079e61aaf0851e6bf461ee6fbb51bf54d
Travis CI User <travis@example.org>
2021-06-09 09:28:56 +0000

    Merge pull request #52 from resoluteCoder/add_uuid_to_image_table
    Add UUID column to `Manage Image` table

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.4b55626dbf57f627cda7.js.map
+//# sourceMappingURL=../sourcemaps/446.8a8a2aa36b80c38da771.js.map
