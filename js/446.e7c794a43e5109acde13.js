8c559b08ee8ccfa7d90b4c2a23616baf9ff126a1
Travis CI User <travis@example.org>
2021-06-10 13:27:43 +0000

    Add Detail page to Image Manager (#53)
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.8a8a2aa36b80c38da771.js.map
+//# sourceMappingURL=../sourcemaps/446.b6fb76fe2fd546c6623a.js.map
--- js/665.d8ab3c7da14be18850c2.js
+++ js/665.d8ab3c7da14be18850c2.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[665],{28368:(n,t,r)=>{var e=r(90939);n.exports=function(n,t,r){var o=(r="function"==typeof r?r:void 0)?r(n,t):void 0;return void 0===o?e(n,t,void 0,r):!!o}},30236:(n,t,r)=>{var e=r(42980),o=r(21463)((function(n,t,r,o){e(n,t,r,o)}));n.exports=o}}]);
-//# sourceMappingURL=../sourcemaps/665.8a8a2aa36b80c38da771.js.map
+//# sourceMappingURL=../sourcemaps/665.b6fb76fe2fd546c6623a.js.map
