af81c227f202f306fd7a5274fa73390e3caa4e19
Travis CI User <travis@example.org>
2021-06-16 16:44:56 +0000

    Merge pull request #56 from sean797/master
    Rename edge to RHEL for Edge for image create wizard

--- js/184.5a25aea45589dea75163.js
+++ js/184.5a25aea45589dea75163.js
@@ -1,3 +1,3 @@
 /*! For license information please see 184.5a25aea45589dea75163.js.LICENSE.txt */
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[184],{94184:(r,e)=>{var t;!function(){"use strict";var n={}.hasOwnProperty;function s(){for(var r=[],e=0;e<arguments.length;e++){var t=arguments[e];if(t){var a=typeof t;if("string"===a||"number"===a)r.push(t);else if(Array.isArray(t)&&t.length){var p=s.apply(null,t);p&&r.push(p)}else if("object"===a)for(var i in t)n.call(t,i)&&t[i]&&r.push(i)}}return r.join(" ")}r.exports?(s.default=s,r.exports=s):void 0===(t=function(){return s}.apply(e,[]))||(r.exports=t)}()}}]);
-//# sourceMappingURL=../sourcemaps/184.e8c26272883ac86be52e.js.map
+//# sourceMappingURL=../sourcemaps/184.582cd1f3541973ff1497.js.map
