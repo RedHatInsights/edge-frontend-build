bd92e9ef313e7fc28e52daa242f1d4b6f61723dd
Travis CI User <travis@example.org>
2021-06-17 15:39:58 +0000

    Add primary toolbar to managed image table (#57)
    The toolbar has the following:
    	* Action button to create a new image
    	* Pagination
    
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- js/412.735d0268ade46ea8e06b.js
+++ js/412.735d0268ade46ea8e06b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[412],{28412:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>f});var n=r(1427),a=r.n(n),o=r(45697),s=r.n(o),i=r(16530),l=r(80962),u=r(30319),c=r(87999),p=r(35240),g=r(94500),h=r.n(g),d=function(e){e.hasLogger;var t=(0,u.S)(h());return a().createElement(u.g.Provider,{value:{getRegistry:function(){return t}}},a().createElement(l.Provider,{store:t.getStore()},a().createElement(i.BrowserRouter,{basename:(0,p.eb)(window.location.pathname,1)},a().createElement(c.Z,null))))};d.propTypes={hasLogger:s().bool};const f=d}}]);
-//# sourceMappingURL=../sourcemaps/412.ea94b6b3c3e3568f083f.js.map
+//# sourceMappingURL=../sourcemaps/412.eb0282edbf1f7c6cf314.js.map
