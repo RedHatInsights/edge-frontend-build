559d598c08093af51f94b22ecc4e5bf44172c81a
Travis CI User <travis@example.org>
2021-06-17 11:50:16 +0000

    Merge pull request #58 from Hyperkid123/add-edge-sass-prefix
    Add edge sass prefix.

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.582cd1f3541973ff1497.js.map
+//# sourceMappingURL=../sourcemaps/446.8e42d06a49d3b0fbd08e.js.map
