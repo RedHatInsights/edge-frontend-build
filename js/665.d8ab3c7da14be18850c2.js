19ad9bee20c560f882e5b558c71f37d79d368318
Travis CI User <travis@example.org>
2021-06-23 14:23:38 +0000

    Fix filter key is invalid error in fleet-management (#61)
    * Fix filter key is invalid error in fleet-management
    
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>
    
    * Fix linting 😟
    
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- js/665.d8ab3c7da14be18850c2.js
+++ js/665.d8ab3c7da14be18850c2.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[665],{28368:(n,t,r)=>{var e=r(90939);n.exports=function(n,t,r){var o=(r="function"==typeof r?r:void 0)?r(n,t):void 0;return void 0===o?e(n,t,void 0,r):!!o}},30236:(n,t,r)=>{var e=r(42980),o=r(21463)((function(n,t,r,o){e(n,t,r,o)}));n.exports=o}}]);
-//# sourceMappingURL=../sourcemaps/665.1a5bc1605d8dc6c1237a.js.map
+//# sourceMappingURL=../sourcemaps/665.7b7a4529ffdced0bdc60.js.map
