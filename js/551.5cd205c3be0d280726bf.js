c0e72aab5561576fa3c6dd1f26ba59c8c3a335ed
Travis CI User <travis@example.org>
2021-06-24 08:12:22 +0000

    Merge pull request #62 from RedHatInsights/master
    Update prod-beta to newest master

--- js/551.5cd205c3be0d280726bf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[551],{68551:(n,t,e)=>{var r=e(22858),s=e(46860),p=e(60379),a=e(80521);n.exports=function(n){return r(n)||s(n)||p(n)||a()}}}]);
