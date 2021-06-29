3509a57fd640c5bd0132356422c911656c0788ba
Travis CI User <travis@example.org>
2021-06-29 15:49:03 +0000

    Merge pull request #64 from matoval/managedImageSortColumns
    Update managed image columns

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.7b7a4529ffdced0bdc60.js.map
+//# sourceMappingURL=../sourcemaps/446.b98106093c31911a0fdc.js.map
