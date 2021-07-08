5a9bc7c69ec30d1b3c22fdc4a08c01afdc263a14
Travis CI User <travis@example.org>
2021-07-08 10:53:48 +0000

    changed sortColumn to sort_by for api query (#76)

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.72628518516f9392e2ce.js.map
+//# sourceMappingURL=../sourcemaps/446.eb32a7748124e0b9a736.js.map
