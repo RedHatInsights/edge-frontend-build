432014b746bfd4149c6575849a7fa687ce013155
Travis CI User <travis@example.org>
2021-07-09 05:05:21 +0000

    Fix breadcrumb typo (#83)

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.617a35f0958ba525ddd3.js.map
+//# sourceMappingURL=../sourcemaps/446.eb97403ffbb49a6c6f88.js.map
