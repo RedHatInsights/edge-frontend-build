274ec4c3fa2323c72a421b2757fc49ce73dd518a
Travis CI User <travis@example.org>
2021-07-11 06:04:05 +0000

    Modify StatusLabel component according Marvelapp Model (#85)
    Modify StatusLabel component to remove borders, background and align items according Marvelapp model
    
    Signed-off-by: Adelia Ferreira <tay.figueira@gmail.com>

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.eb97403ffbb49a6c6f88.js.map
+//# sourceMappingURL=../sourcemaps/446.0f2e0e24aeb6087a9cdd.js.map
