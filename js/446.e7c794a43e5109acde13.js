ce4535b484121470201c0866897d2d515452306f
Travis CI User <travis@example.org>
2021-07-14 19:52:39 +0000

    Update image details tab (#89)

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.c1cbbb01d43ee11592b1.js.map
+//# sourceMappingURL=../sourcemaps/446.ded7be43dd4101166ac9.js.map
