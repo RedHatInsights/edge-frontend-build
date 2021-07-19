24201c66ca5ee04cb891bd8d2d34e9c379d2e2a7
Travis CI User <travis@example.org>
2021-07-19 05:53:22 +0000

    Refactor managed images to fix 3 bugs: (#101)
    * Do not re-render toolbar on filtering results
    * On no match show "No match" empty state
    * Remove image type from filters as it is not supported on API
    
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- js/412.17a2139c2ed2c2d28bc1.js
+++ js/412.17a2139c2ed2c2d28bc1.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[412],{28412:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>f});var n=r(1427),a=r.n(n),o=r(45697),s=r.n(o),i=r(16530),l=r(80962),u=r(32511),c=r(87999),p=r(35240),g=r(94500),h=r.n(g),d=function(e){e.hasLogger;var t=(0,u.S)(h());return a().createElement(u.g.Provider,{value:{getRegistry:function(){return t}}},a().createElement(l.Provider,{store:t.getStore()},a().createElement(i.BrowserRouter,{basename:(0,p.eb)(window.location.pathname,1)},a().createElement(c.Z,null))))};d.propTypes={hasLogger:s().bool};const f=d}}]);
-//# sourceMappingURL=../sourcemaps/412.cb4c509601035e820557.js.map
+//# sourceMappingURL=../sourcemaps/412.c8d422e2fa5630dba7b3.js.map
