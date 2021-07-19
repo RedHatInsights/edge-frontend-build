1792a4f988675f024d2d27764619d35243e9db18
Travis CI User <travis@example.org>
2021-07-19 05:53:31 +0000

    Refactor managed images to fix 3 bugs: (#101)
    * Do not re-render toolbar on filtering results
    * On no match show "No match" empty state
    * Remove image type from filters as it is not supported on API
    
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.cb4c509601035e820557.js.map
+//# sourceMappingURL=../sourcemaps/446.c8d422e2fa5630dba7b3.js.map
