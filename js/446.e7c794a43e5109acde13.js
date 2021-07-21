515cdcc359737b8b713a9d91a2defff9ae07e9fa
Travis CI User <travis@example.org>
2021-07-21 13:00:10 +0000

    Add Device Status column: Running, Booting, Unspecified (#106)
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.2883755fadf9db9da3a1.js.map
+//# sourceMappingURL=../sourcemaps/446.06a0bbe06870ec1f55b8.js.map
