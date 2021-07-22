a8ee7f00a7e1fa05cd29c0ad1126173b0c786ad5
Travis CI User <travis@example.org>
2021-07-22 19:17:09 +0000

    Change SimpleListItem to text, added custom styles to download button (#109)

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.554486de9d6abf462e5f.js.map
+//# sourceMappingURL=../sourcemaps/446.14905572129f6b1115a9.js.map
