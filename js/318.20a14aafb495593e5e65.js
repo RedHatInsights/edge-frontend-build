7520b0358d8759b89cdff17a2957312e4f6fe0f0
Travis CI User <travis@example.org>
2021-08-04 18:08:24 +0000

    Merge pull request #121 from matoval/refresh-images-on-update
    fixed updateWizard updating images on update

--- js/318.20a14aafb495593e5e65.js
+++ js/318.20a14aafb495593e5e65.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[318],{95318:e=>{e.exports=function(e){return e&&e.__esModule?e:{default:e}}}}]);
-//# sourceMappingURL=../sourcemaps/318.c7ad22168d5e2844ee44.js.map
+//# sourceMappingURL=../sourcemaps/318.79b18436846f2e51d4d0.js.map
