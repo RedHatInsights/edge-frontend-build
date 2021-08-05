024a9472606abd8b99c1f47254f74bac9170cdb5
Travis CI User <travis@example.org>
2021-08-05 15:55:48 +0000

    Merge pull request #115 from boaz0/closes_90237
    Fix pagination errors

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.79b18436846f2e51d4d0.js.map
+//# sourceMappingURL=../sourcemaps/446.7cfbc02f1ace4d5ba25c.js.map
