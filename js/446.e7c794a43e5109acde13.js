04789b3785df4b5a7fc77cef9e26824e153b414b
Travis CI User <travis@example.org>
2021-08-09 19:30:06 +0000

    Merge pull request #122 from resoluteCoder/wizard-v2
    Wizard v2 updates

--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.749d159b742cd8ae517a.js.map
+//# sourceMappingURL=../sourcemaps/446.308ea1042abc97152170.js.map
