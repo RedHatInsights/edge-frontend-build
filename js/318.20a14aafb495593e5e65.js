dd605e4bced79e8cd25b4e75329115f45bf5addc
Travis CI User <travis@example.org>
2021-08-11 16:20:43 +0000

    Merge pull request #129 from resoluteCoder/back-to-commit
    switch from update to commit text

--- js/318.20a14aafb495593e5e65.js
+++ js/318.20a14aafb495593e5e65.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[318],{95318:e=>{e.exports=function(e){return e&&e.__esModule?e:{default:e}}}}]);
-//# sourceMappingURL=../sourcemaps/318.c21d2190dbfe600b714c.js.map
+//# sourceMappingURL=../sourcemaps/318.b6d5497c1a8e3eed49b4.js.map
