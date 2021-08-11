7a06ebc8d7c2de0c5a7968f6c7832659676cd9f6
Travis CI User <travis@example.org>
2021-08-11 16:20:34 +0000

    Merge pull request #129 from resoluteCoder/back-to-commit
    switch from update to commit text

--- js/412.6a95a8e809a3a6394351.js
+++ js/412.6a95a8e809a3a6394351.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[412],{28412:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>f});var n=r(1427),a=r.n(n),o=r(45697),s=r.n(o),i=r(16530),l=r(80962),u=r(59245),c=r(87999),p=r(35240),g=r(94500),h=r.n(g),d=function(e){e.hasLogger;var t=(0,u.S)(h());return a().createElement(u.g.Provider,{value:{getRegistry:function(){return t}}},a().createElement(l.Provider,{store:t.getStore()},a().createElement(i.BrowserRouter,{basename:(0,p.eb)(window.location.pathname,1)},a().createElement(c.Z,null))))};d.propTypes={hasLogger:s().bool};const f=d}}]);
-//# sourceMappingURL=../sourcemaps/412.c21d2190dbfe600b714c.js.map
+//# sourceMappingURL=../sourcemaps/412.b6d5497c1a8e3eed49b4.js.map
--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.c21d2190dbfe600b714c.js.map
+//# sourceMappingURL=../sourcemaps/446.b6d5497c1a8e3eed49b4.js.map
