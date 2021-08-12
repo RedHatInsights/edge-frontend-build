e28714c40aa1139cf4218a045f309d13d1dba38b
Travis CI User <travis@example.org>
2021-08-12 17:29:47 +0000

    Merge pull request #130 from resoluteCoder/fix-repourl-api-call
    fixed ostreeparent commit issue

--- js/318.20a14aafb495593e5e65.js
+++ js/318.20a14aafb495593e5e65.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[318],{95318:e=>{e.exports=function(e){return e&&e.__esModule?e:{default:e}}}}]);
-//# sourceMappingURL=../sourcemaps/318.b6d5497c1a8e3eed49b4.js.map
+//# sourceMappingURL=../sourcemaps/318.376a50c4995ac512f128.js.map
--- js/446.e7c794a43e5109acde13.js
+++ js/446.e7c794a43e5109acde13.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[446],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}}}]);
-//# sourceMappingURL=../sourcemaps/446.b6d5497c1a8e3eed49b4.js.map
+//# sourceMappingURL=../sourcemaps/446.376a50c4995ac512f128.js.map
