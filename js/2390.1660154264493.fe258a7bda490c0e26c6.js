445bf8d8184f7bcd07beab1f253e815226c6ee78
Travis CI User <travis@example.org>
2022-08-10 18:01:21 +0000

    Merge pull request #520 from RedHatInsights/changed-title-to-Camel-case
    changed ImageInformationCard title to Camel case

---
+++ js/2390.1660154264493.fe258a7bda490c0e26c6.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[2390,5467],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}},45467:()=>{}}]);
