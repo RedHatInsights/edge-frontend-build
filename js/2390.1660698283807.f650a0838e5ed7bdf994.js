d49a8aa21ac1b667a38c4328a36be61250acb502
Travis CI User <travis@example.org>
2022-08-17 15:37:54 +0000

    Merge pull request #526 from pavankrao/fix-device-display-text
    Fix UI display text from device to system

--- js/2390.1660698283807.f650a0838e5ed7bdf994.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[2390,5467],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}},45467:()=>{}}]);
-//# sourceMappingURL=../sourcemaps/2390.806a578abcc85397ecb767ab8df2e364.js.map
