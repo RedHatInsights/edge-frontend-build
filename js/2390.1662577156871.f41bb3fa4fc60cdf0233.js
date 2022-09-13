0fef40aeb301b46a88ba8b922d54154f82f7711c
Travis CI User <travis@example.org>
2022-09-13 17:43:41 +0000

    Merge pull request #542 from tpapaioa/fix_custom_repo_table_sort
    Fix table sorting after modal submit for custom repositories

--- js/2390.1662577156871.f41bb3fa4fc60cdf0233.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[2390,5467],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}},45467:()=>{}}]);
-//# sourceMappingURL=../sourcemaps/2390.453573d20fec0bbe6e2923ef17e46490.js.map
