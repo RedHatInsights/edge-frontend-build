43950903dfd3703ee7799625de3132c57a7fd5b5
Travis CI User <travis@example.org>
2022-09-20 20:59:47 +0000

    Merge pull request #553 from de1987/fix-unresponsive-message-and-uppercase
    Fix unresponsive message and uppercase

--- js/2390.1663706341168.5405161ae5ade48922a7.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[2390,5467],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}},45467:()=>{}}]);
-//# sourceMappingURL=../sourcemaps/2390.6aefbbadeb5d652c9358452c6b2ba06b.js.map
