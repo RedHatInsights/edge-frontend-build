d421f62f02b62e62e42c90cc6d26d6f5ceeef477
Travis CI User <travis@example.org>
2022-09-29 16:09:58 +0000

    Merge pull request #534 from daphnemaeve/add-update-button
    Add update button to inventory

---
+++ js/2390.1664467557226.44b66e9a09841853a6c2.js
@@ -0,0 +1,2 @@
+(self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[2390,5467],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}},45467:()=>{}}]);
+//# sourceMappingURL=../sourcemaps/2390.32b997415a9c4001ab475a52ead785fe.js.map
