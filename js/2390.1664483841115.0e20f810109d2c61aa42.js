67c72a9c8afa66a70d16ffd5bb4005ac578200b6
Travis CI User <travis@example.org>
2022-10-04 17:07:28 +0000

    Merge pull request #563 from pavankrao/id-for-inventory-device-update
    Add id to DeviceTable toolbar components

--- js/2390.1664483841115.0e20f810109d2c61aa42.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[2390,5467],{18446:(n,t,e)=>{var r=e(90939);n.exports=function(n,t){return r(n,t)}},45467:()=>{}}]);
-//# sourceMappingURL=../sourcemaps/2390.0df8a35a6e309d1427ea28dcd3b065ce.js.map
